e54bf455d45d021a5bdfe251e731416d6209a1e4
Magdalena Jarzynka <magdalena.jarzynka@gmail.com>
2020-07-07 13:08:03 +0200

    Add First Unique Character In A String solution

---
+++ src/com/madzia/FirstUniqueCharacterInAString.java
@@ -0,0 +1,36 @@
+package com.madzia;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class FirstUniqueCharacterInAString {
+    public int firstUniqChar(String s) {
+        char[] chars = s.toCharArray();
+        Set<Character> uniqueCharacterSet = new LinkedHashSet<>();
+        Set<Character> repeatingCharacterSet = new HashSet<>();
+        int result = -1;
+
+        for (Character character : chars) {
+            if (!uniqueCharacterSet.contains(character) && !repeatingCharacterSet.contains(character)) {
+                uniqueCharacterSet.add(character);
+            } else if (uniqueCharacterSet.contains(character)) {
+                repeatingCharacterSet.add(character);
+                uniqueCharacterSet.remove(character);
+            }
+        }
+        if (!uniqueCharacterSet.isEmpty()) {
+            Character firstUniqueCharacter = uniqueCharacterSet.stream().findFirst().get();
+            int i = 0;
+            while (i < chars.length) {
+                if (chars[i] == firstUniqueCharacter) {
+                    result = i;
+                    break;
+                } else {
+                    i++;
+                }
+            }
+        }
+        return result;
+    }
+}
--- src/com/madzia/Main.java
+++ src/com/madzia/Main.java
@@ -5,13 +5,16 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        ValidPalindrome validPalindrome = new ValidPalindrome();
-        System.out.println(validPalindrome.isPalindrome("A man, a plan, a canal: Panama"));
+//        ValidPalindrome validPalindrome = new ValidPalindrome();
+//        System.out.println(validPalindrome.isPalindrome("A man, a plan, a canal: Panama"));
+//
+//        SingleNumber singleNumber = new SingleNumber();
+//        System.out.println(singleNumber.singleNumber(new int[]{4, 1, 2, 1, 2}));
+//
+//        ReverseString reverseString = new ReverseString();
+//        reverseString.reverseString(new char[]{'a', 'B', 'c', 'd', 'E', 'f', '0'});
 
-        SingleNumber singleNumber = new SingleNumber();
-        System.out.println(singleNumber.singleNumber(new int[]{4, 1, 2, 1, 2}));
-
-        ReverseString reverseString = new ReverseString();
-        reverseString.reverseString(new char[]{'a', 'B', 'c', 'd', 'E', 'f', '0'});
+        FirstUniqueCharacterInAString firstUniqueCharacterInAString = new FirstUniqueCharacterInAString();
+        System.out.println(firstUniqueCharacterInAString.firstUniqChar("loveleetcodevtc"));
     }
 }
