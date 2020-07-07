4d880b5f719fe469f871112824334952ba076df5
Magdalena Jarzynka <magdalena.jarzynka@gmail.com>
2020-07-07 15:26:00 +0200

    Add Merge Two Sorted Lists solution

--- src/com/madzia/Main.java
+++ src/com/madzia/Main.java
@@ -1,5 +1,7 @@
 package com.madzia;
 
+import com.madzia.MergeTwoSortedLists.ListNode;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,8 +15,30 @@ public class Main {
 //
 //        ReverseString reverseString = new ReverseString();
 //        reverseString.reverseString(new char[]{'a', 'B', 'c', 'd', 'E', 'f', '0'});
+//
+//        FirstUniqueCharacterInAString firstUniqueCharacterInAString = new FirstUniqueCharacterInAString();
+//        System.out.println(firstUniqueCharacterInAString.firstUniqChar("loveleetcodevtc"));
 
-        FirstUniqueCharacterInAString firstUniqueCharacterInAString = new FirstUniqueCharacterInAString();
-        System.out.println(firstUniqueCharacterInAString.firstUniqChar("loveleetcodevtc"));
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(4);
+
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+
+        ListNode listNode4 = new ListNode(1);
+        ListNode listNode5 = new ListNode(3);
+        ListNode listNode6 = new ListNode(4);
+
+        listNode4.next = listNode5;
+        listNode5.next = listNode6;
+
+        MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+
+        ListNode x = mergeTwoSortedLists.mergeTwoLists(listNode1, listNode4);
+        while (x != null) {
+            System.out.println(x.val);
+            x = x.next;
+        }
     }
 }
---
+++ src/com/madzia/MergeTwoSortedLists.java
@@ -0,0 +1,41 @@
+package com.madzia;
+
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode resultNode = new ListNode();
+        ListNode currentNode = resultNode;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                currentNode.next = l1;
+                l1 = l1.next;
+            } else {
+                currentNode.next = l2;
+                l2 = l2.next;
+            }
+            currentNode = currentNode.next;
+        }
+        if (l1 == null) {
+            currentNode.next = l2;
+        } else {
+            currentNode.next = l1;
+        }
+        return resultNode.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
