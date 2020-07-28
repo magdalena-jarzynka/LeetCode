9445eb3ff9405d6064b45976a494672436102fc2
Magdalena Jarzynka <magdalena.jarzynka@gmail.com>
2020-07-28 16:43:01 +0200

    Extract methods in Valid Parentheses solution

--- src/main/java/com/madzia/ValidParentheses.java
+++ src/main/java/com/madzia/ValidParentheses.java
@@ -3,22 +3,34 @@ package com.madzia;
 import java.util.Stack;
 
 public class ValidParentheses {
+    private Stack<Character> characterStack = new Stack<>();
+
     public boolean isValid(String s) {
         char[] chars = s.toCharArray();
-        Stack<Character> characterStack = new Stack<>();
         for (char character : chars) {
-            if (character == '{' || character == '(' || character == '[') {
+            if (isOpeningBracket(character)) {
                 characterStack.push(character);
-            } else if (character == '}' && !characterStack.empty() && characterStack.peek() == '{') {
-                characterStack.pop();
-            } else if (character == ')' && !characterStack.empty() && characterStack.peek() == '(') {
-                characterStack.pop();
-            } else if (character == ']' && !characterStack.empty() && characterStack.peek() == '[') {
+            } else if (isBracketsMatch(character)) {
                 characterStack.pop();
             } else {
                 return false;
             }
         }
         return characterStack.empty();
+    }
+
+    private boolean isBracketsMatch(char character, char opening, char closing) {
+        return character == closing && characterStack.peek() == opening;
+    }
+
+    private boolean isBracketsMatch(char character) {
+        return !characterStack.empty()
+                && (isBracketsMatch(character, '{', '}')
+                || isBracketsMatch(character, '(', ')')
+                || isBracketsMatch(character, '[', ']'));
+    }
+
+    private boolean isOpeningBracket (char character) {
+        return character == '{' || character == '(' || character == '[';
     }
 }
