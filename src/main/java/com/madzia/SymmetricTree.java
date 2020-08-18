c79e16465b5fb474a1b6a3fff4f2eb4e58437de6
Magdalena Jarzynka <magdalena.jarzynka@gmail.com>
2020-08-18 14:04:07 +0200

    Add Symmetric Tree solution

---
+++ src/main/java/com/madzia/SymmetricTree.java
@@ -0,0 +1,41 @@
+package com.madzia;
+
+/**
+ * Solution for <a href="https://leetcode.com/problems/symmetric-tree/">Symmetric Tree</a>
+ */
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        } else if (root1 == null || root2 == null) {
+            return false;
+        }
+        return root1.val == root2.val && isSymmetric(root1.left, root2.right) && isSymmetric(root1.right, root2.left);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
