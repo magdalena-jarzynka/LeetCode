ed237a4b2db1cb02c3fc9c7af3bdbb486978e65b
Magdalena Jarzynka <magdalena.jarzynka@gmail.com>
2020-11-10 18:08:36 +0100

    Add Fibonacci Number solution with faster runtime

--- src/main/java/com/madzia/FibonacciNumber.java
+++ src/main/java/com/madzia/FibonacciNumber.java
@@ -4,13 +4,30 @@ package com.madzia;
  * Solution for <a href="https://leetcode.com/problems/fibonacci-number/">Fibonacci Number</a>
  */
 public class FibonacciNumber {
+
+    private int n;
+
     public int fib(int n) {
+        this.n = n;
+
         if (n == 0) {
             return 0;
         }
         if (n == 1) {
             return 1;
         }
-        return fib(n - 1) + fib(n - 2);
+        return fib(2, 1, 0);
+    }
+
+    private int fib(int i, int nMinusOne, int nMinusTwo) {
+        int sum = nMinusOne + nMinusTwo;
+        if (i == n) {
+            return sum;
+        } else {
+            nMinusTwo = nMinusOne;
+            nMinusOne = sum;
+            i++;
+            return fib(i, nMinusOne, nMinusTwo);
+        }
     }
 }
